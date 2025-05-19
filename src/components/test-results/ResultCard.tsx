efe579e5ee6b9cb4719f16d691aee6b741c3f54a
ytbpremiumsh <ytbpremshbn827@gmail.com>
2025-05-19 09:54:43 +0700

    src/components/test-results/ResultCard.tsx

--- src/components/test-results/ResultCard.tsx
+++ src/components/test-results/ResultCard.tsx
@@ -20,8 +20,8 @@ const ResultCard: React.FC<ResultCardProps> = ({
           <span className="inline-block px-4 py-2 rounded-full bg-white text-mbti-deep-purple font-semibold mb-4">
             Tipe Kepribadianmu
           </span>
-          <h1 className="text-4xl sm:text-5xl font-bold mb-2">{result.type}</h1>
-          <h2 className="text-2xl sm:text-3xl">{result.title}</h2>
+          <h2 className="text-4xl sm:text-5xl font-bold mb-2">{result.type}</h2> - <h2 className="text-2xl sm:text-3xl">{result.title}</h2>
+          
         </div>
         
         <CardContent className="p-6 px-[18px] py-[4px]">
