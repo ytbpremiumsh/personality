01290a954dd9fc54d4aac412e9529ee890fa26f7
ytbpremiumsh <ytbpremshbn827@gmail.com>
2025-05-19 12:08:09 +0700

    src/components/test-results/ResultCard.tsx

--- src/components/test-results/ResultCard.tsx
+++ src/components/test-results/ResultCard.tsx
@@ -27,9 +27,6 @@ const ResultCard: React.FC<ResultCardProps> = ({
         <CardContent className="p-6 px-[18px] py-[4px]">
           <div className="mb-6 max-w-4l mx-auto">
             <div className="flex items-center mb-4 justify-center">
-              <Separator className="flex-grow bg-mbti-deep-purple h-[1px]" />
-              <h3 className="text-xl font-semibold mx-2">Deskripsi</h3>
-              <Separator className="flex-grow bg-mbti-deep-purple h-[1px]" />
             </div>
             <p className="text-gray-700 text-sm sm:text-base text-justify">{result.description}</p>
           </div>
